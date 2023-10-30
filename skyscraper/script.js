290e6930bb342166d74c2b8e410f2e518ac1afae
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-10-30 17:09:15 +0800

    Play music and show gif once wire is snapped into place

--- skyscraper/script.js
+++ skyscraper/script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let delay = 300;
 
-  // Each span in h1 eLement will slide in
+  // Each span within the h1 element will slide in
   span.forEach((el) => {
     animateElementWithDelay(el, delay, "left", "0", "opacity", "1");
     delay += 300;
@@ -61,6 +61,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // Makes wire image draggable using custom drag and drop
     let cOffX = 0;
     let cOffY = 0;
+    let snapThreshold = 40;
+    const audio = document.getElementById("song");
+    const gif = document.getElementById("pepe-gif");
 
     wire.addEventListener("mousedown", dragStart);
 
@@ -81,11 +84,43 @@ document.addEventListener("DOMContentLoaded", () => {
       wire.style.left = (e.clientX - cOffX).toString() + "px";
     }
 
+    /*  This function will do three things for every mouseup:
+    remove drag and drop event listeners from the document,
+    snap wire into place if near target location,
+    and play music if the wire is in target location */
     function dragEnd(e) {
       e.preventDefault();
 
       document.removeEventListener("mousemove", dragMove);
       document.removeEventListener("mouseup", dragEnd);
+
+      // Calculate distance to the headphone jack
+      const targetX = 97;
+      const targetY = 175;
+      const currentX = parseInt(wire.style.left, 10);
+      const currentY = parseInt(wire.style.top, 10);
+      const distance = Math.sqrt(
+        Math.pow(targetX - currentX, 2) + Math.pow(targetY - currentY, 2)
+      );
+
+      // Check if the distance is within the snap threshold to snap element in position
+      if (distance <= snapThreshold) {
+        wire.style.left = targetX.toString() + "px";
+        wire.style.top = targetY.toString() + "px";
+
+        // Once element is in position, play music and show gif
+        startTheParty();
+
+        // Remove the drag function from wire
+        wire.removeEventListener("mousedown", dragStart);
+      }
+    }
+
+    function startTheParty() {
+      audio.play();
+      setTimeout(() => {
+        gif.style.display = "block";
+      }, 3500);
     }
   });
 });
